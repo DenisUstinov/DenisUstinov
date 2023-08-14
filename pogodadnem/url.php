da65a16016715cc3b3fe15593b9eb855935212f5
Denis Ustinow <ml_engineer@icloud.com>
2023-08-15 06:34:44 +0700

    Rename project pogoda-dnem

--- pogodadnem/url.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define('DIRECTORI', __DIR__); // Для путей PHP
-	define('HOSTS','http://'.$_SERVER['HTTP_HOST']); // Для ссылок HTML
-	define('DOMAINS', $_SERVER['HTTP_HOST']); // Для ссылок HTML
-?>
