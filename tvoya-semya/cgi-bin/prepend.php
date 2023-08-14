b1798a4990a64959a3c997800ec7e608fcdbf3dd
Denis Ustinow <ml_engineer@icloud.com>
2023-08-14 12:31:09 +0700

    Remove unnecessary files

--- tvoya-semya/cgi-bin/prepend.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (preg_match('/^tideways/', $_SERVER['SERVER_NAME']) && !preg_match('/xhprof/', $_SERVER['REQUEST_URI'])) {
-        tideways_enable(TIDEWAYS_FLAGS_CPU+TIDEWAYS_FLAGS_MEMORY);
-}
-
